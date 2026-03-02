288cb8cca2489e64f95d229c50a565769f3e3926
amineedarkaoui <amineed98@gmail.com>
2026-03-02 20:55:24 +0000

    Created Product Repository

---
+++ src/main/java/ma/amine/gestion_produits/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package ma.amine.gestion_produits.repository;
+
+import ma.amine.gestion_produits.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Long, Product> {
+}
