723ea643285a0b3125b09599dfad1f754805154b
amineedarkaoui <amineed98@gmail.com>
2026-03-03 05:55:48 +0000

    Tested Product Repository

--- src/main/java/ma/amine/gestion_produits/GestionProduitsApplication.java
+++ src/main/java/ma/amine/gestion_produits/GestionProduitsApplication.java
@@ -1,7 +1,12 @@
 package ma.amine.gestion_produits;
 
+import ma.amine.gestion_produits.entity.Product;
+import ma.amine.gestion_produits.repository.ProductRepository;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GestionProduitsApplication {
@@ -10,4 +15,26 @@ public class GestionProduitsApplication {
         SpringApplication.run(GestionProduitsApplication.class, args);
     }
 
+    @Bean
+    ApplicationRunner applicationRunner(ProductRepository productRepository) {
+        return args -> {
+            productRepository.save(Product.builder()
+                    .name("TV")
+                    .price(500.0)
+                    .build()
+            );
+            productRepository.save(Product.builder()
+                    .name("Hello")
+                    .price(500.0)
+                    .build()
+            );
+            productRepository.save(Product.builder()
+                    .name("World")
+                    .price(500.0)
+                    .build()
+            );
+
+            productRepository.findAll().forEach(p -> System.out.println(p));
+        };
+    }
 }
