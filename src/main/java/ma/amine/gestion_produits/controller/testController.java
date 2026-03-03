e0b495f793b281642fa2f63502b0cc157547e10c
amineedarkaoui <amineed98@gmail.com>
2026-03-03 11:17:40 +0000

    Turned off Spring Security

---
+++ src/main/java/ma/amine/gestion_produits/controller/testController.java
@@ -0,0 +1,13 @@
+package ma.amine.gestion_produits.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return ("Hello World!");
+    }
+}
